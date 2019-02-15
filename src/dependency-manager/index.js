c2528dda0b4d31eff16c9dcdb497ccd813d8a9de
Mercier, Jérémie <jeremie.mercier@mastercard.com>
2019-02-15 16:40:50 +0100

    Reduce emphasis on antiCorruptionLayer; Pronostic: node is dead by 2022

--- src/dependency-manager/index.js
+++ src/dependency-manager/index.js
@@ -1,5 +1,5 @@
-const WiretreeImplementation = require('./implementations/wiretree-adapter');
+const WtImplementation = require('./implementations/wt-adapter');
 
 // Export the implementation of your choice;
 // Wiretree has proven perfect and fast so far.
-module.exports = WiretreeImplementation;
+module.exports = WtImplementation;
